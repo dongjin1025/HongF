4346e61876976745628d7c384fbf5d4a3f327109
hp\user <kimwinner90@gmail.com>
2017-01-14 19:25:55 +0900

    Item_Story layout

--- app/src/main/java/com/dongjin/android/hongf/MainActivity.java
+++ app/src/main/java/com/dongjin/android/hongf/MainActivity.java
@@ -2,40 +2,26 @@ package com.dongjin.android.hongf;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
 
 import com.dongjin.android.hongf.storelist.ListFragment;
-import com.yalantis.contextmenu.lib.ContextMenuDialogFragment;
-import com.yalantis.contextmenu.lib.MenuObject;
-import com.yalantis.contextmenu.lib.MenuParams;
-import com.yalantis.contextmenu.lib.interfaces.OnMenuItemClickListener;
-import com.yalantis.contextmenu.lib.interfaces.OnMenuItemLongClickListener;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import devlight.io.library.ntb.NavigationTabBar;
 
-public class MainActivity extends AppCompatActivity implements OnMenuItemClickListener, OnMenuItemLongClickListener {
+public class MainActivity extends AppCompatActivity implements mainView {
 
-    private ViewPager pager;
-    private TabLayout tab;
+
     private FragmentManager fragmentManager;
     private MapFragment mapFragment;
     private StoryFragment storyFragment;
     private ListFragment listFragment;
-    private ContextMenuDialogFragment mMenuDialogFragment;
+
 
 
     @Override
@@ -48,15 +34,13 @@ public class MainActivity extends AppCompatActivity implements OnMenuItemClickLi
         storyFragment=new StoryFragment();
         listFragment=new ListFragment();
 
-        initToolbar();
-        initUI();
-        initMenuFragment();
+
 
 
     }
 
-
-    private void initUI() {
+    @Override
+    public void setPager() {
         final ViewPager viewPager = (ViewPager) findViewById(R.id.vp_horizontal_ntb);
 
 
@@ -82,6 +66,11 @@ public class MainActivity extends AppCompatActivity implements OnMenuItemClickLi
                 return fragment;
             }
         });
+
+    }
+
+    @Override
+    public void setTab() {
 
         final String[] colors = getResources().getStringArray(R.array.france);
 
@@ -135,101 +124,8 @@ public class MainActivity extends AppCompatActivity implements OnMenuItemClickLi
                 }
             }
         }, 500);
-    }
-
-    private void initToolbar() {
-        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        TextView mToolBarTextView = (TextView) findViewById(R.id.text_view_toolbar_title);
-        setSupportActionBar(mToolbar);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setHomeButtonEnabled(true);
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setDisplayShowTitleEnabled(false);
-        }
-        mToolbar.setNavigationIcon(R.drawable.btn_back);
-        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
-        mToolBarTextView.setText("연재형님의 LOGO~~");
-    }
-    private void initMenuFragment() {
-        MenuParams menuParams = new MenuParams();
-        menuParams.setActionBarSize((int) getResources().getDimension(R.dimen.tool_bar_height));
-        menuParams.setMenuObjects(getMenuObjects());
-        menuParams.setClosableOutside(false);
-        mMenuDialogFragment = ContextMenuDialogFragment.newInstance(menuParams);
-        mMenuDialogFragment.setItemClickListener(this);
-        mMenuDialogFragment.setItemLongClickListener(this);
-    }
-    private List<MenuObject> getMenuObjects() {
-
-        // item.setTextColor(...)
-        // item.setDividerColor(...)
-
-        List<MenuObject> menuObjects = new ArrayList<>();
-
-        MenuObject close = new MenuObject();
-        close.setResource(R.drawable.btn_popup_close);
-
-        MenuObject profile = new MenuObject("My page");
-        profile.setResource(R.drawable.ic_account_24dp);
-
-        MenuObject search = new MenuObject("Search");
-        search.setResource(R.drawable.icon_topbar_search_unselect);
-
-        MenuObject proposal = new MenuObject("To DONGJIN");
-        proposal.setResource(R.drawable.ic_profile_added_rp_tips);
-
-
-
-
-        menuObjects.add(close);
-        menuObjects.add(profile);
-        menuObjects.add(search);
-        menuObjects.add(proposal);
-
-        return menuObjects;
-    }
-    @Override
-    public void onBackPressed() {
-        if (mMenuDialogFragment != null && mMenuDialogFragment.isAdded()) {
-            mMenuDialogFragment.dismiss();
-        } else {
-            finish();
-        }
-    }
-
-
-
-    @Override
-    public void onMenuItemClick(View clickedView, int position) {
 
     }
-
-    @Override
-    public void onMenuItemLongClick(View clickedView, int position) {
-
-    }
-    @Override
-    public boolean onCreateOptionsMenu(final Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.context_menu:
-                if (fragmentManager.findFragmentByTag(ContextMenuDialogFragment.TAG) == null) {
-                    mMenuDialogFragment.show(fragmentManager, ContextMenuDialogFragment.TAG);
-                }
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 }
+
+
---
+++ app/src/main/java/com/dongjin/android/hongf/mainView.java
@@ -0,0 +1,11 @@
+package com.dongjin.android.hongf;
+
+/**
+ * Created by user on 2017-01-12.
+ */
+
+public interface mainView {
+
+    void setPager();
+    void setTab();
+}
